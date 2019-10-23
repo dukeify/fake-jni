f28762649730b92adfc2aca0920f454a9d822b71
Matthewacon <matthewjbarichello@gmail.com>
2019-10-22 21:16:48 -0400

    Began work on API utilities, related to #57

---
+++ src/fake-jni/fake-jni.cpp
@@ -0,0 +1,48 @@
+#include <cstring>
+
+#include "fake-jni/fake-jni.h"
+
+#define _CERATE_MAIN_METHOD_CHECK \
+auto log = vm.getLog();\
+JClass * clazz;\
+if (!cclazz) {\
+ clazz = createDummyClass(vm);\
+} else {\
+ clazz = const_cast<JClass *>(cclazz);\
+ for (auto mid : clazz->getMethods()) {\
+  if (strcmp(mid->getName(), "main") == 0 && strcmp(mid->getSignature(), "([Ljava/lang/String;)V") == 0) {\
+   fprintf(log, "WARNING: '%s' already contains a main method definition!\n", clazz->getName());\
+   return;\
+  }\
+ }\
+}
+
+namespace FakeJni {
+ JClass * createDummyClass(const Jvm & vm) {
+  std::string dummyName = "__Dummy";
+  bool nameOverlaps = true;
+  while (nameOverlaps) {
+   nameOverlaps = false;
+   for (auto& clazz : vm.getClasses()) {
+    if (strcmp(clazz->getName(), dummyName.c_str()) == 0) {
+     dummyName.push_back('_');
+     nameOverlaps = true;
+     break;
+    }
+   }
+  }
+  return new JClass(dummyName.c_str(), JClass::PUBLIC);
+ }
+
+ void createMainMethod(const Jvm & vm, _CX::main_method_t * main, JClass * cclazz) {
+  _CERATE_MAIN_METHOD_CHECK
+  clazz->registerMethod();
+  vm.registerClass(clazz);
+ }
+
+ void createMainMethod(const Jvm & vm, std::function<_CX::main_method_t> main, const JClass * cclazz) {
+  _CERATE_MAIN_METHOD_CHECK
+  clazz->registerMethod(new JMethodID());
+  vm.registerClass(clazz);
+ }
+}
