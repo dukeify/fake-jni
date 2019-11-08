8f879270cf968ca3c6aad4956badf8d66d20aa7d
Matthewacon <matthewjbarichello@gmail.com>
2019-11-08 16:56:50 -0500

    Added JNIEXPORT macro to jni agent and module functions

--- examples/src/agent.cpp
+++ examples/src/agent.cpp
@@ -4,16 +4,19 @@
 
 extern "C" {
  //Example dynamic Agent linkage
+ JNIEXPORT
  jint Agent_OnLoad(JavaVM * vm, char * options, void * reserved) {
   std::cout << "Cold Start Agent Hello World!" << std::endl;
   return JNI_OK;
  }
 
+ JNIEXPORT
  jint Agent_OnAttach(JavaVM * vm, char * options, void * reserved) {
   std::cout << "Live Attach Agent Hello World!" << std::endl;
   return JNI_OK;
  }
 
+ JNIEXPORT
  void Agent_OnUnload(JavaVM * vm) {
   std::cout << "Agent Goodbye World!" << std::endl;
  }
--- examples/src/jni.cpp
+++ examples/src/jni.cpp
@@ -4,11 +4,13 @@
 
 extern "C" {
  //Example dynamic JNI linkage
+ JNIEXPORT
  jint JNI_OnLoad(JavaVM * vm, void * reserved) {
   std::cout << "JNI Hello World!" << std::endl;
   return JNI_OK;
  }
 
+ JNIEXPORT
  void JNI_OnUnload(JavaVM * vm, void * reserved) {
   std::cout << "JNI Goodbye World!" << std::endl;
  }
